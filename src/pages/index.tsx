c0ebffbf23d5fed9b69b8b8ea09fe54e05bdd091
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-09-22 23:54:22 -0500

    showing top headlines and descriptions on main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { getNews } from '../services/api'
+import { AxiosPromise } from 'axios';
+
 
 const HomePage = () => {
+
+  const [news, setNews] = useState<any>()
+
+  const fetchNews = async () => {
+    const topNews = await getNews();
+    setNews(topNews)
+    console.log("ooo this is news: " + JSON.stringify(topNews))
+
+  }
+
+  useEffect(() => {
+    fetchNews()
+  }, [])
+
+
   return (
-    <div>
+    <div className="flex min-h-screen min-w-screen items-center mt-10">
+      {news?.articles.map((article: any) => {
+
+        return(
+          <div key={article.id} className="mt-20 w-3/4">
+            <h4 className="text-red-500">{article.title}</h4>
+            <p>{article.description ? article.description : article.url}</p>
+          </div>
+        )
+      })}
       <h1>Welcome to My Next.js App</h1>
       <p>This is the main page of my Next.js app.</p>
     </div>
