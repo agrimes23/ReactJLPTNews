99ec09b844402ac2830db9b859c2c9c6319c6c7f
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-09-23 01:03:58 -0500

    bringing in n4 kanji list that is in the description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,18 @@
 import React, {useState, useEffect} from 'react';
-import { getNews } from '../services/api'
+import { getNews, search } from '../services/api'
 import { AxiosPromise } from 'axios';
 
 
 const HomePage = () => {
-
+  const [kanjiList, setKanjiList] = useState<any>()
   const [news, setNews] = useState<any>()
 
   const fetchNews = async () => {
     const topNews = await getNews();
+    const searchKanji = await search();
     setNews(topNews)
-    console.log("ooo this is news: " + JSON.stringify(topNews))
-
+    setKanjiList(searchKanji)
+    console.log("oohhh Kanji: " + JSON.stringify(kanjiList))
   }
 
   useEffect(() => {
@@ -23,10 +24,11 @@ const HomePage = () => {
     <div className="bg-purple-500 flex min-h-screen min-w-screen items-center mt-10">
       {news?.articles.map((article: any) => {
 
+
         return(
           <div key={article.id} className="mt-20 w-3/4">
-            <h4 className="text-red-500">{article.title}</h4>
-            <p>{article.description ? article.description : article.url}</p>
+            <h4 style={{color: "red"}}>{article.title}</h4>
+            <p>{article.description}</p>
           </div>
         )
       })}
