403a36944ae4bc8e25a6bcd8035c96779b626320
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-09-24 17:28:49 -0500

    was able to separate each word in the description, still working on getting it to display on the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,64 @@
-import React, {useState, useEffect} from 'react';
-import { getNews, search } from '../services/api'
-import { AxiosPromise } from 'axios';
+import React, { useState, useEffect } from 'react';
+import { getNews, search } from '../services/api';
 
+interface Article {
+  id: number;
+  title: string;
+  description: string;
+}
 
-const HomePage = () => {
-  const [kanjiList, setKanjiList] = useState<any>()
-  const [news, setNews] = useState<any>()
+interface Kanji {
+  word: string;
+}
+
+const HomePage: React.FC = () => {
+  const [kanjiList, setKanjiList] = useState<Kanji[]>([]);
+  const [news, setNews] = useState<{ articles: Article[] }>({ articles: [] });
 
   const fetchNews = async () => {
-    const topNews = await getNews();
-    const searchKanji = await search();
-    setNews(topNews)
-    setKanjiList(searchKanji)
-    console.log("oohhh Kanji: " + JSON.stringify(kanjiList))
-  }
+    try {
+      const topNews = await getNews();
+      const searchKanji = await search();
+      setNews(topNews);
+      setKanjiList(searchKanji);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
   useEffect(() => {
-    fetchNews()
-  }, [])
+    fetchNews();
+  }, []);
 
+  const formatArticleDescription = (description: string) => {
+    // ✅ wordsInDescription works!
+    // wordsInDescription is an array of words from that description
+    const wordsInDescription = description.split(/(\p{Script=Hiragana}+|\p{Script=Katakana}+|[一-龯々]+)/u); // Split based on Japanese characters
+    const formattedDescription: JSX.Element[] = [];
+
+    wordsInDescription.forEach((word, index) => {
+      if (kanjiList.some((kanji) => kanji.word === word)) {
+        formattedDescription.push(
+          <span key={index} style={{ color: 'red' }}>
+            ***{word}***
+          </span>
+        );
+      } else {
+        formattedDescription.push(<React.Fragment key={index}>{word}</React.Fragment>);
+      }
+    });
+
+    return formattedDescription;
+  };
 
   return (
     <div className="bg-purple-500 flex min-h-screen min-w-screen items-center mt-10">
-      {news?.articles.map((article: any) => {
-          // TODO: // 1. loop through articles
-                // 2. Make function that:
-                  // - loops through each word in the article
-                  // - compares each article word to the words in the kanjiList array
-                // 3. if it does equal, then return that word in a span with the color red
-                    // - if it doesn't equal, return that word as normal black color
-
-
-        return(
-          <div key={article.id} className="mt-20 w-3/4">
-            <h4 style={{color: "red"}}>{article.title}</h4>
-            <p>{article.description}</p>
-          </div>
-        )
-      })}
-      
+      {news.articles.map((article, index) => (
+        <div key={index} className="mt-20 w-3/4">
+          <h4 style={{ color: 'red' }}>{article.title}</h4>
+          <p>{formatArticleDescription(article.description)}</p>
+        </div>
+      ))}
     </div>
   );
 };
