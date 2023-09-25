d5d6719adae954cdf048f81105042f83a69049b9
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-09-25 13:12:06 -0500

    added some todo items, and wrote notes on how it's being looped through so I can do it again and reformat it later

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,16 @@ interface Kanji {
   word: string;
 }
 
+//TODO: Make two functions??? One separating sentences into individual words and another comparing kanji???
+// 1. bring in other vocab levels and do samething as n4 vocab in the backend
+// 2. bring that information to the frontend
+// 3. do the same loop through for each level and make them different colors based on level
+// 4. style the home page
+// 5. Can we get the full article?? research
+// 6. make a "need to study vocab" page??? (full project, including backend)
+// 7. build out flash cards so users can practice (full project, including backend)
+
+
 const HomePage: React.FC = () => {
   const [kanjiList, setKanjiList] = useState<Kanji[]>([]);
   const [news, setNews] = useState<{ articles: Article[] }>({ articles: [] });
@@ -38,29 +48,37 @@ const HomePage: React.FC = () => {
     const formattedDescription: JSX.Element[] = [];
 
     
-
+    // some are blank and will cause the site to crash, so this if statement is necessary to keep the site running
     if(wordsInDescription) {
-    wordsInDescription.forEach((word: any, index: number) => {
-      if (kanjiList.some((kanji) => kanji === word)) {
-        formattedDescription.push(
-          <span key={index} className="text-red-500 font-bold" style={{ color: 'red' }}>
-            {word}
-          </span>
-        );
+      // loop through each word in description and compare
+      wordsInDescription.forEach((word: any, index: number) => {
+        // .some() will return a boolean, and compares if the conditions passes the test provided by the function (anonymous arrow function in this case)
+        // this loops through the kanjiList array and checks if the kanji matches each word in the description
+        // if it is true, it adds the text as bolded and red in the array
+        if (kanjiList.some((kanji) => kanji === word)) {
+          formattedDescription.push(
+            <span key={index} className="text-red-500 font-bold" style={{ color: 'red' }}>
+              {word}
+            </span>
+          );
       } else {
+        // if it is false, it adds the text as it was (black text color) in the array
         formattedDescription.push(<React.Fragment key={index}>{word}</React.Fragment>);
       }
     });
   }
-
+    // then it returns the array. Since they're all span or react fragments going into an jsx element array, they are all displayed inline
     return formattedDescription;
   };
 
   return (
     <div className="flex flex-col min-h-screen min-w-screen items-center my-10">
+
+      {/* loop through each top headline title and description to display */}
       {news.articles.map((article, index) => (
         <div key={index} className="mt-20 w-3/4">
           <h4 className="text-green-500 text-xl">{article.title}</h4>
+          {/* function to loop through kanjiList and each word in top headline description */}
           <p className="">{formatArticleDescription(article.description)}</p>
         </div>
       ))}
