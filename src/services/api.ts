8553725812bce201a2823adbc4c76ba19834c956
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-10-13 22:01:11 -0500

    checking once more

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-});
-
 // const api = axios.create({
-//   baseURL: "http://dontpushthistomain:5000/api",
+//   baseURL: "http://localhost:5000/api",
 // });
+
+const api = axios.create({
+  baseURL: "http://dontpushthistomain:5000/api",
+});
 
 export const getNews = async () => {
   const response = await api.get('/news');
