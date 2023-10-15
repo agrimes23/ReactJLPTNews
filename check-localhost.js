28fcc49d13b22af54b6837ebdbe75f67401a6d8c
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-10-15 15:35:06 -0500

    testing again lets see

--- check-localhost.js
+++ check-localhost.js
@@ -5,9 +5,9 @@ const fileContent = fs.readFileSync(filePath, 'utf8');
 
 // Check for uncommented localhost references
 if (fileContent.includes('localhost')) {
-  console.error('Uncommented localhost reference found.');
+  console.error('PLEASE FIX: Uncommented localhost reference found.');
   process.exit(1); // Return a non-zero exit code to indicate a failed test
 } else {
-  console.log('No uncommented localhost references found.');
+  console.log('Okay to push: No uncommented localhost references found.');
   process.exit(0); // Return zero to indicate a successful test
 }
