ba0bfa1a790672ffa12135b5ece675373492ac06
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-10-15 15:16:57 -0500

    testing again

---
+++ check-localhost.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const filePath = 'path/to/your-specific-file.js'; // Update with the actual file path
+const fileContent = fs.readFileSync(filePath, 'utf8');
+
+// Check for uncommented localhost references
+if (fileContent.includes('localhost')) {
+  console.error('Uncommented localhost reference found.');
+  process.exit(1); // Return a non-zero exit code to indicate a failed test
+} else {
+  console.log('No uncommented localhost references found.');
+  process.exit(0); // Return zero to indicate a successful test
+}
