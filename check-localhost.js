e6f8f059ed0ae84bb23ef712b265b70719f036c4
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-10-15 15:35:12 -0500

    testing again lets see

--- check-localhost.js
+++ check-localhost.js
@@ -4,7 +4,7 @@ const filePath = 'src/services/api.ts'; // Update with the actual file path
 const fileContent = fs.readFileSync(filePath, 'utf8');
 
 // Check for uncommented localhost references
-if (fileContent.includes('localhost')) {
+if (!fileContent.includes('//   baseURL: "http://localhost')) {
   console.error('PLEASE FIX: Uncommented localhost reference found.');
   process.exit(1); // Return a non-zero exit code to indicate a failed test
 } else {
