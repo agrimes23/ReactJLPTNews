0ed7e412d69c602939f0050fba2242cb8d65aff1
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-10-15 15:13:53 -0500

    testing

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,7 @@ const api = axios.create({
 });
 
 // const api = axios.create({
-//   baseURL: "http://dontpushthistomain:5000/api",
+//   baseURL: "http://okaytopushthistomain:5000/api",
 // });
 
 export const getNews = async () => {
